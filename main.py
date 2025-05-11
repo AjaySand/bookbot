35c5f4e62f0c9184a2e4fde2c2288a319f7d30ba
Ajaydeep Sandhu <ajaysandhu@twentic.com>
2025-05-11 19:37:33 +0200

    mini refactor

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(path):
+    with open(path) as f:
+        file_contets = f.read()
+
+    return file_contets;
+
+def main():
+    boot_text = get_book_text('./books/frankenstein.txt')
+    num_words = get_num_words(boot_text)
+    print(str(num_words) + ' words found in the document')
+
+main()
